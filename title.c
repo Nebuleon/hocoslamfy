8bac3ac4386d7c09977351eb5c7379249aa474a6
Ivan <hi-ban@hotmail.com>
2014-02-10 03:06:55 +0100

    Add scrolling landscape and bee in title screen

--- title.c
+++ title.c
@@ -26,6 +26,18 @@
 #include "game.h"
 #include "title.h"
 #include "text.h"
+#include "SDL_image.h"
+
+static int BGCounterA = 0;
+static int BGCounterB = 0;
+static int BGCounterC = 0;
+static int BGCounterD = 0;
+static int BGCounterE = 0;
+static int CounterB = 0;
+static int CounterC = 0;
+static int CounterD = 0;
+static int CounterE = 0;
+static int BeeCounter = -60;
 
 static bool WaitingForRelease = false;
 
@@ -69,7 +81,173 @@ void TitleScreenDoLogic(bool* Continue, bool* Error)
 
 void TitleScreenOutputFrame()
 {
-	SDL_FillRect(Screen, NULL, SDL_MapRGB(Screen->format, 0, 48, 0));
+	//Draw the background E.
+	SDL_Rect BGRectE;
+			BGRectE.x=BGCounterE;
+			BGRectE.y=54;
+			BGRectE.w=320;
+			BGRectE.h=138;
+	SDL_Rect BGRectEdst;
+			BGRectEdst.x=0;
+			BGRectEdst.y=54;
+			BGRectEdst.w=320;
+			BGRectEdst.h=138;
+	if (CounterE < 4) {
+		CounterE++;
+	} else {
+		CounterE = 0;
+		if (BGCounterE < 160) {
+			BGCounterE++;
+		} else {
+			BGCounterE = 0;
+		}
+	}
+	SDL_BlitSurface(frames_bg, &BGRectE, Screen, &BGRectEdst);
+	
+	//Draw the background D.
+	SDL_Rect BGRectD;
+			BGRectD.x=BGCounterD;
+			BGRectD.y=192;
+			BGRectD.w=320;
+			BGRectD.h=8;
+	SDL_Rect BGRectDdst;
+			BGRectDdst.x=0;
+			BGRectDdst.y=192;
+			BGRectDdst.w=320;
+			BGRectDdst.h=8;
+	if (CounterD < 3) {
+		CounterD++;
+	} else {
+		CounterD = 0;
+		if (BGCounterD < 160) {
+			BGCounterD++;
+		} else {
+			BGCounterD = 0;
+		}
+	}
+	SDL_BlitSurface(frames_bg, &BGRectD, Screen, &BGRectDdst);
+	
+	//Draw the background C.
+	SDL_Rect BGRectC;
+			BGRectC.x=BGCounterC;
+			BGRectC.y=42;
+			BGRectC.w=320;
+			BGRectC.h=12;
+	SDL_Rect BGRectCdst;
+			BGRectCdst.x=0;
+			BGRectCdst.y=42;
+			BGRectCdst.w=320;
+			BGRectCdst.h=12;
+	SDL_Rect BGRectC2;
+			BGRectC2.x=BGCounterC;
+			BGRectC2.y=200;
+			BGRectC2.w=320;
+			BGRectC2.h=10;
+	SDL_Rect BGRectC2dst;
+			BGRectC2dst.x=0;
+			BGRectC2dst.y=200;
+			BGRectC2dst.w=320;
+			BGRectC2dst.h=10;
+	if (CounterC < 2) {
+		CounterC++;
+	} else {
+		CounterC = 0;
+		if (BGCounterC < 160) {
+			BGCounterC++;
+		} else {
+			BGCounterC = 0;
+		}
+	}
+	SDL_BlitSurface(frames_bg, &BGRectC, Screen, &BGRectCdst);
+	SDL_BlitSurface(frames_bg, &BGRectC2, Screen, &BGRectC2dst);
+	
+	//Draw the background B.
+	SDL_Rect BGRectB;
+			BGRectB.x=BGCounterB;
+			BGRectB.y=28;
+			BGRectB.w=320;
+			BGRectB.h=14;
+	SDL_Rect BGRectBdst;
+			BGRectBdst.x=0;
+			BGRectBdst.y=28;
+			BGRectBdst.w=320;
+			BGRectBdst.h=14;
+	SDL_Rect BGRectB2;
+			BGRectB2.x=BGCounterB;
+			BGRectB2.y=210;
+			BGRectB2.w=320;
+			BGRectB2.h=12;
+	SDL_Rect BGRectB2dst;
+			BGRectB2dst.x=0;
+			BGRectB2dst.y=210;
+			BGRectB2dst.w=320;
+			BGRectB2dst.h=12;
+	if (CounterB < 1) {
+		CounterB++;
+	} else {
+		CounterB = 0;
+		if (BGCounterB < 160) {
+			BGCounterB++;
+		} else {
+			BGCounterB = 0;
+		}
+	}
+	SDL_BlitSurface(frames_bg, &BGRectB, Screen, &BGRectBdst);
+	SDL_BlitSurface(frames_bg, &BGRectB2, Screen, &BGRectB2dst);
+	
+	//Draw the background A.
+	SDL_Rect BGRectA;
+			BGRectA.x=BGCounterA;
+			BGRectA.y=0;
+			BGRectA.w=320;
+			BGRectA.h=28;
+	SDL_Rect BGRectAdst;
+			BGRectAdst.x=0;
+			BGRectAdst.y=0;
+			BGRectAdst.w=320;
+			BGRectAdst.h=28;
+	SDL_Rect BGRectA2;
+			BGRectA2.x=BGCounterA;
+			BGRectA2.y=222;
+			BGRectA2.w=320;
+			BGRectA2.h=18;
+	SDL_Rect BGRectA2dst;
+			BGRectA2dst.x=0;
+			BGRectA2dst.y=222;
+			BGRectA2dst.w=320;
+			BGRectA2dst.h=18;
+	if (BGCounterA < 160) {
+		BGCounterA++;
+	} else {
+		BGCounterA = 0;
+	}
+	SDL_BlitSurface(frames_bg, &BGRectA, Screen, &BGRectAdst);
+	SDL_BlitSurface(frames_bg, &BGRectA2, Screen, &BGRectA2dst);
+	
+	// Draw the character.
+	SDL_Rect PlayerPixels;
+		PlayerPixels.x = -60;
+		PlayerPixels.y = (SCREEN_HEIGHT - (126 * SCREEN_HEIGHT / FIELD_HEIGHT) -1);
+		PlayerPixels.w = (int) (PLAYER_SIZE * SCREEN_HEIGHT / FIELD_HEIGHT) +2;
+		PlayerPixels.h = (int) (PLAYER_SIZE * SCREEN_HEIGHT / FIELD_HEIGHT) +2;
+	if (BeeCounter < 600) {
+		PlayerPixels.x=BeeCounter;
+		BeeCounter++;
+	} else {
+		PlayerPixels.x=-60;
+		BeeCounter=-60;
+	}
+	SDL_Rect P1Rect;
+			P1Rect.x=0;
+			P1Rect.y=0;
+			P1Rect.w=16;
+			P1Rect.h=16;
+	if (CounterB < 1) {
+		P1Rect.x=16;
+	} else {
+		P1Rect.x=0;
+	}
+	SDL_BlitSurface(frames_player, &P1Rect, Screen, &PlayerPixels);
 
 	if (SDL_MUSTLOCK(Screen))
 		SDL_LockSurface(Screen);
